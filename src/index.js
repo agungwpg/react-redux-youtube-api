e692f205f9eaf725edc168bc1077923be6ab2ead
Agung Wira Putra Gea <agungwpg@gmail.com>
2018-09-21 22:29:28 +0700

    section2 episode 9, passing props 2 levels

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAFsOri-_OXzFwhVQJb6gJqiWR0lYGeRZw';
 
-// create a new component. This component should produce some HTML
-const App = () => {
-    return (
-    <div>
-        <SearchBar/>
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        YTSearch({key:API_KEY,term:'twenty one pilots'},(videos)=>{
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+            }); //this.setState({videos:videos});
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    videos={this.state.videos}
+                    onVideoSelect={(selectedVideo)=>this.setState({selectedVideo})}
+                />
+            </div>
+        );
+    }
 }
-// Take this component's generated HTML and put it on the page (in the DOM)
+
 ReactDOM.render(<App/>,document.querySelector('.container'));
